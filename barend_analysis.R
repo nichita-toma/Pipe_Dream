1ae6c30d53bc4632ca9e3f30abfeece86f06b92d
Barend Nooren <barend.nooren@gmail.com>
2026-01-21 14:27:33 +0100

    aggregation of activists and life expectancy DRAFT

--- barend_analysis.R
+++ barend_analysis.R
@@ -1,3 +1,4 @@
+#Read data
 library(tidyverse)
 
 activists_df <-  read_csv("data_activists")
@@ -6,12 +7,17 @@ life_expectancy_data <- read_csv("data_life_expectancy")
 
 democratic_index_data <- read_csv("data_democratic_index")
 
-#calculate the mean value of the lifespan
+#Calculate the mean value of the lifespan
 life_expectancy_df_mean <-  life_expectancy_data |>
-  mutate(life_expectancy_average = 1950-1955 + 1955-1960 + 1960-1965 + 1965-1970 + 1970-1975 + 1975-1980)
+  mutate(life_expectancy_average = 1950-1955 + 1955-1960 + 1960-1965 + 1965-1970 + 1970-1975 + 1975-1980) |>
   select(country, life_expectancy_mean)
 
-#give a label of democratic-non-democratic for each country
+#Add average life expectancy to activist data
+activists_with_LE <- activists_df |>
+  left_join(life_expectancy_df_mean, 
+  by = c("country_origin" = "country")) #Here, "country_origin" is the countries as listed in activist data, "country" is from the life expectancy data
+
+#Give a label of democratic-non-democratic for each country
 lifespan_df <- life_expectancy_df_mean |>
   mutate(democracy_status = ifelse(democratic_index >= 0.6, democratic, non-democratic)) |>
   group_by('democracy_status')
