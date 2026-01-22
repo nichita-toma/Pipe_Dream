5b0fc9e17a2971202b50c01264b4ce3586a871a5
Nichita Danilov <danilovnichita@gmail.com>
2026-01-22 14:38:43 +0100

    full data combined with data set about democracy index and life expectancy

--- Nichita_try_R.R
+++ Nichita_try_R.R
@@ -14,17 +14,28 @@ life_expectancy_data <- read_csv("life-expectancy.csv")|>
   rename(life_expectancy = `Life expectancy (years)`, country_col = Entity)
   #print()
 
-democratic_index_data <- read_csv("Human-Progress-Liberal-Democracy-Index.csv")|>
+democratic_index_data <- read_csv("Human-Progress-Liberal-Democracy-Index.csv")
 
-  pivot_longer(c(`1789`:`2024`), names_to = "Year", values_to = "Index")|>
-  print()
+# This works! Everthing is converted to numeric, you can work with the dataset now :) let me know if you want me to explain what this code does! -Radost
+for (column in names(democratic_index_data)){
+  if (column != "country_name"){
+    democratic_index_data[[column]] <- as.numeric(democratic_index_data[[column]])}
+  }
+print(democratic_index_data)
+
+democratic_index_data_long <- pivot_longer(democratic_index_data, c(`1789`:`2024`), names_to = "Year", values_to = "Index")|>
+  mutate(Year_int = as.numeric(Year))|>
+  select(country_name, Year_int, Index)
+
+ # print(democratic_index_data_long)
 
 
 #Pivot life expectancy data
 
-activist_w_life_expect <- activists_df_rough |>
-  left_join(life_expectancy_data, by = c("Country" = "country_col", "Death_year_int" = "Year"))
-#print()
+ activist_w_life_expect <- activists_df_rough |>
+   left_join(life_expectancy_data, by = c("Country" = "country_col", "Death_year_int" = "Year"))
+ #print()
 
-activist_df_complete <- activist_w_life_expect |>
-  left_join()
+ activist_df_complete <- activist_w_life_expect |>
+   left_join(democratic_index_data_long, by = c("Country" = "country_name", "Death_year_int" = "Year_int"))|>
+print()
